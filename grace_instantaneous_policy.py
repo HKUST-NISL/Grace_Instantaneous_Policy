82796fe51357e06bc3ab474c5332a25a628fcd1c
SHEN, Yifan <41409072+TheDoctor-JI@users.noreply.github.com>
2023-07-01 20:43:06 +0800

    Added read function

---
+++ grace_instantaneous_policy.py
@@ -0,0 +1,91 @@
+#general
+import yaml
+import rospy
+import os
+import re
+import threading
+from signal import signal
+from signal import SIGINT
+import logging
+import sys
+from datetime import datetime
+import time
+from inspect import getsourcefile
+from os.path import abspath
+from statemachine import StateMachine, State
+
+#ros
+import dynamic_reconfigure.client
+import sensor_msgs.msg
+import std_msgs.msg
+import hr_msgs.msg
+import grace_attn_msgs.msg
+import grace_attn_msgs.srv
+import hr_msgs.msg
+import hr_msgs.cfg
+import hr_msgs.srv
+import std_msgs
+
+
+#Load configs
+def loadConfig(path):
+    #Load configs
+    with open(path, "r") as config_file:
+        config_data = yaml.load(config_file, Loader=yaml.FullLoader)
+        # print("Config file loaded")
+    return config_data
+
+#Create Logger
+def setupLogger(file_log_level, terminal_log_level, logger_name, log_file_name):
+    log_formatter = logging.Formatter('%(asctime)s %(msecs)03d %(name)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s', 
+                                  datefmt='%d/%m/%Y %H:%M:%S')
+
+    f = open(log_file_name, "a")
+    f.close()
+    file_handler = logging.FileHandler(log_file_name)
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(file_log_level)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(log_formatter)
+    stream_handler.setLevel(terminal_log_level)
+
+    logger = logging.getLogger(logger_name)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    logger.setLevel( min(file_log_level,terminal_log_level) )#set to lowest
+
+    return logger
+
+#Respond to exit signal
+def handle_sigint(signalnum, frame):
+    # terminate
+    print('Main interrupted! Exiting.')
+    sys.exit()
+
+
+
+
+class InstantaneousPolicy(StateMachine):
+
+    def __init__(self):
+        #miscellaneous
+        signal(SIGINT, handle_sigint)
+        self.__logger = setupLogger(
+                    logging.DEBUG, 
+                    logging.INFO, 
+                    self.__class__.__name__,
+                    "./logs/log_" + datetime.now().strftime("%a_%d_%b_%Y_%I_%M_%S_%p"))
+
+        path = abspath(getsourcefile(lambda:0)) + "./config/config.yaml"
+        self.__config_data = loadConfig(path)
+
+
+    def applyPolicy(self, state_pace, state_turn):
+        pass
+
+
+
+
+
+
